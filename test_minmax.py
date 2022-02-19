dbb8cd9f8f85d15ee123a0160de8f3850625a5cd
siddhart19 <77846186+siddhart19@users.noreply.github.com>
2022-02-19 20:04:22 +0530

    Add files via upload

---
+++ test_minmax.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+root.title("Fever_Report")
+root.geometry("400x00")
+
+label1=label(root,text="months:""january""febuary""march""april""may""june""july""august""september""october""november""december")
+label2=label(root,text="profits:""20000""45000""78000""97000""12000""456000""65000""54000""10000""30000""70000""90000")
+label3=label(root,text="maximum profit:""456000 was in the month of june")
+label4=label(root,text="minimum profit:""10000 was in the month of september")
